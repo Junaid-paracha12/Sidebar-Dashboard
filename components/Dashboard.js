27f1b653abb979acc73da82e5db7dca31ea9d29b
Junaid-paracha-007 <junaidmaqbool009@gmail.com>
2024-02-21 20:56:15 +0500

    update component

--- components/Dashboard.js
+++ components/Dashboard.js
@@ -7,20 +7,20 @@ export default function Dashboard() {
     <div className='grid grid-cols-1 md:grid-cols-2 gap-6'>
        <div className='flex flex-col gap-6'>
         <div className='relative'>
-            <Image src={"/pic1.jpg"} width={300} height={30} className='h-[35vh] w-full rounded-md' />
+            <Image src={"/pic1.jpg"} width={300} height={30} className='h-[35vh] w-full rounded-md object-cover' />
             <div className='absolute inset-0 flex items-center'>
                <h1 className='text-xl md:text-2xl lg:text-3xl font-semibold max-w-28 p-3'>Seater Sofa</h1>
             </div>
         </div>
         <div className='grid grid-cols-1 md:grid-cols-2 gap-6'>
          <div className='relative'>
-            <Image src={"/pic2.jpg"} width={300} height={300} className='h-[55vh] w-full rounded-md' />
+            <Image src={"/pic2.jpg"} width={300} height={300} className='h-[55vh] w-full rounded-md object-cover' />
            <div className='absolute inset-0 flex items-center'>
             <h1 className='text-xl md:text-2xl lg:text-3xl font-semibold max-w-28 p-3'>Stool & Benches</h1>
            </div>
          </div>
          <div className='relative'>
-            <Image src={"/pic2.jpg"} width={300} height={300} className='h-[55vh] w-full rounded-md' />
+            <Image src={"/pic4.jpg"} width={300} height={300} className='h-[55vh] w-full rounded-md object-cover' />
            <div className='absolute inset-0 flex items-center'>
             <h1 className='text-xl md:text-2xl lg:text-3xl font-semibold max-w-28 p-3'>Stool Chair</h1>
            </div>
@@ -31,20 +31,21 @@ export default function Dashboard() {
        <div className='flex flex-col gap-6'>
         <div className='grid grid-cols-1 md:grid-cols-2 gap-6'>
          <div className='relative'>
-            <Image src={"/pic2.jpg"} width={300} height={300} className='h-[55vh] w-full rounded-md' />
+            <Image src={"/pic3.jpg"} width={300} height={300} className='h-[55vh] w-full rounded-md object-cover' />
            <div className='absolute inset-0 flex items-center'>
             <h1 className='text-xl md:text-2xl lg:text-3xl font-semibold max-w-28 p-3'>Plumage Hand</h1>
            </div>
          </div>
          <div className='relative'>
-            <Image src={"/pic2.jpg"} width={300} height={300} className='h-[55vh] w-full rounded-md' />
+            <Image src={"/pic4.jpg"} width={300} height={300} className='h-[55vh] w-full rounded-md object-cover' />
            <div className='absolute inset-0 flex items-center'>
             <h1 className='text-xl md:text-2xl lg:text-3xl font-semibold max-w-28 p-3'>Dinning Chair</h1>
            </div>
          </div>
 
-        </div> <div className='relative'>
-            <Image src={"/pic1.jpg"} width={300} height={30} className='h-[35vh] w-full rounded-md' />
+        </div> 
+        <div className='relative'>
+            <Image src={"/pic5.jpg"} width={300} height={30} className='h-[35vh] w-full rounded-md object-cover' />
             <div className='absolute inset-0 flex items-center'>
                <h1 className='text-xl md:text-2xl lg:text-3xl font-semibold max-w-28 p-3'>Accent Chair</h1>
             </div>
--- components/Sidebar.js
+++ components/Sidebar.js
@@ -4,29 +4,37 @@ import Dashboard from "./Dashboard";
 export default function Sidebar (){
   const [open, setOpen] = useState(true);
   const Menus = [
-    { title: "Inbox", src: "/Chat.png" },
+    
     { title: "Dashboard", src: "/dashboard.svg" },
     { title: "Calender", src: "/calender.svg", gap: true },
     { title: "Document ", src: "/chat.svg" },
     { title: "Team", src: "/user.svg" },
-    { title: "Document", src: "/Folder.png" },
-    { title: "Files ", src: "/Folder.png", gap: true },
-    { title: "Setting", src: "/Setting.png" },
+    { title: "Trash", src: "/trash.png" },
+    { title: "Files ", src: "/Folder.png",  },
+    { title: "More ", src: "/more.png",  },
+    { title: "Help & Support", src: "/Settings.png", gap: true },
   ];
 
   return (
-    <div className="flex">
+    <div className="flex w-full">
+      <div className="bg-slate-950 h-screen fixed overflow-y-scroll scroll-smooth scroll-hidden">
       <div
         className={` ${
-          open ? "w-72" : "w-20 "
-        } bg-slate-950 h-screen p-5  pt-8 relative duration-300`}
+          open ? "w-60" : "w-20 "
+        }  p-5  pt-8 relative duration-300`}
       >
-       
+        <Image
+        width={20} height={20}
+          src="/control.png"
+          className={`absolute cursor-pointer -right-3 top-9 w-7 border-dark-purple
+           border-2 rounded-full  ${!open && "rotate-180"}`}
+          onClick={() => setOpen(!open)}
+        />
         <div className="flex gap-x-4 items-center">
           <Image
           width={20} height={20}
             src="/1.png"
-            className={`h-5 w-5 cursor-pointer duration-500  rounded-full ${
+            className={`h-7 w-7 cursor-pointer duration-500  rounded-full ${
               open && "rotate-[360deg]"
             }`}
           />
@@ -38,11 +46,21 @@ export default function Sidebar (){
             John Doe
           </h1>
         </div>
-        <ul className="pt-6">
+        <ul className="pt-5">
+         <li className={`flex  rounded-md p-2 cursor-pointer hover:bg-light-white text-gray-300 text-sm items-center gap-x-4  
+              ${open ? "mt-9" : "mt-2"}`}>
+          <Image src={"/chat.png"} width={20} height={20}  className="stroke-white fill-white" />
+          <span className={`${!open && "hidden"} origin-left duration-200`}>
+                Inbox
+              </span>
+              <Image src={"/down.png"} width={10} height={10} className="w-3 h-3" />
+         </li>
+        </ul>
+        <ul className="">
           {Menus.map((Menu, index) => (
             <li
               key={index}
-              className={`flex  rounded-md p-2 cursor-pointer hover:bg-light-white text-gray-300 text-sm items-center gap-x-4 
+              className={`flex  rounded-md p-2 cursor-pointer hover:bg-light-white text-gray-300 text-sm items-center gap-x-4  
               ${Menu.gap ? "mt-9" : "mt-2"} ${
                 index === 0 && "bg-light-white"
               } `}
@@ -51,18 +69,15 @@ export default function Sidebar (){
               <span className={`${!open && "hidden"} origin-left duration-200`}>
                 {Menu.title}
               </span>
+              
             </li>
           ))}
         </ul>
-        <Image
-        width={20} height={20}
-          src="/control.png"
-          className={`absolute cursor-pointer -right-3 top-9 w-7 border-dark-purple
-           border-2 rounded-full  ${!open && "rotate-180"}`}
-          onClick={() => setOpen(!open)}
-        />
-      </div>
-      <div className="h-screen flex-1 p-7">
+       
+      </div></div>
+      <div className={` ${
+          open ? "ml-64" : "ml-24 "
+        } w-full pr-7 pl-3 py-6`}>
         <Dashboard />
       </div>
     </div>
